385e5ae223fdd72a6b82784a591084ad7f566a0b
Pheidippides <pheidippides.github@gmail.com>
2017-11-07 17:13:02 +0100

    changed package name ("-" not permitted)

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(name="COSMO_CLM2_tools",
-      version="0.1",
+setup(name='COSMO_CLM2_tools',
+      version='0.1',
       description="python based tools to set up a cOSMO_CLM2 case",
       author="Matthieu Leclair",
       author_email="matthieu.leclair@env.ethz.ch",
-      url="https://git.iac.ethz.ch/leclairm/pv_cam_fv-dycore",
-      entry_points={'console_scripts': ['cc2_create_case = cosmo_clm2:create_new_case']},
+      url="https://github.com/COSMO-RESM/COSMO-CLM2_tools",
+      packages=['COSMO_CLM2_tools'],
+      entry_points={'console_scripts': ['COSMO_CLM2_tools.cc2_create_case = cosmo_clm2:create_new_case']},
 )
