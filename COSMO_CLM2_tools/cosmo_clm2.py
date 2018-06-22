d21119d3afc1b8272e2ed3102618b663ff01bef9
Matthieu Leclair <matthieu.leclair@env.ethz.ch>
2018-06-22 14:20:57 +0200

    start implementing base_case and daint_case

--- COSMO_CLM2_tools/cosmo_clm2.py
+++ COSMO_CLM2_tools/cosmo_clm2.py
@@ -19,9 +19,402 @@ date_fmt_cosmo = '%Y%m%d%H'
 date_fmt_cesm = '%Y%m%d'
 # preamble:1 ends here
 
-# [[file:~/Projects/COSMO_CLM2_tools/COSMO_CLM2_tools.org::*class%20case][class case:1]]
-class case(object):
+# [[file:~/Projects/COSMO_CLM2_tools/COSMO_CLM2_tools.org::*base%20case%20class][base case class:1]]
+class base_case(object):
     """Class defining a COSMO-CLM2 case"""
+
+    # Class wide variables
+    # ====================
+    # Number of tasks per node
+    _n_tasks_per_node = None
+
+    # ====
+    # Init
+    # ====
+    def __init__(self, name='COSMO_CLM2', path=None,
+                 start_date=None, end_date=None, run_length=None,
+                 COSMO_exe='./cosmo', CESM_exe='./cesm.exe',
+                 wall_time='24:00:00', account=None, partition=None,
+                 login_shell=True,
+                 ncosx=None, ncosy=None, ncosio=None, ncesm=None,
+                 gpu_mode=False, modules_opt=None, pgi_version=None,
+                 dummy_day=True, cosmo_only=False):
+        # Basic init (no particular work required)
+        self.cosmo_only = cosmo_only
+        self.run_length = run_length
+        self.COSMO_exe = COSMO_exe
+        if not self.cosmo_only:
+            self.CESM_exe = CESM_exe
+        self.wall_time = wall_time
+        self.account = account
+        self.gpu_mode = gpu_mode
+        self.modules_opt = modules_opt
+        self.pgi_version = pgi_version
+        self.login_shell = login_shell
+        self.partition = partition
+        self.dummy_day = dummy_day
+        # Settings involving namelist changes
+        self.path = path
+        self.nml = nmldict(self)
+        self.name = name
+        self.start_date = start_date
+        self.end_date = end_date
+        self._compute_run_dates()   # defines _run_start_date, _run_end_date and _runtime (maybe _end_date)
+        self._apply_run_dates()
+        self._check_gribout()
+        self._organize_tasks(ncosx, ncosy, ncosio, ncesm)
+        self.write_open_nml()   # Nothing requires changing namelists after that
+        # Create batch scripts
+        if not self.cosmo_only:
+            self._build_proc_config()
+        self._build_controller()
+        # Create missing directories
+        self._create_missing_dirs()
+        # Write case to xml file
+        self.to_xml('config.xml')
+
+    # Properties
+    # ----------
+    @property
+    def path(self):
+        return self._path
+    @path.setter
+    def path(self, path):
+        if path is None:
+            self._path = os.path.abspath(os.path.join(os.environ['SCRATCH'], self.name))
+        else:
+            self._path = os.path.abspath(path)
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        self._name = name
+        if not self.cosmo_only:
+            self.nml['drv_in']['seq_infodata_inparm']['case_name'] = name
+
+    @property
+    def start_date(self):
+        return self._start_date
+    @start_date.setter
+    def start_date(self, start_date):
+        if start_date is not None:
+            self._start_date = datetime.strptime(start_date, date_fmt_in)
+            self.nml['INPUT_ORG']['runctl']['ydate_ini'] = self._start_date.strftime(date_fmt_cosmo)
+        elif 'ydate_ini' in self.nml['INPUT_ORG']['runctl'].keys():
+            self._start_date = datetime.strptime(self.nml['INPUT_ORG']['runctl']['ydate_ini'],
+                                                 date_fmt_cosmo)
+        else:
+            raise ValueError("ydate_ini has to be given in INPUT_ORG/runctl if no start_date is provided")
+
+    @property
+    def end_date(self):
+        return self._end_date
+    @end_date.setter
+    def end_date(self, end_date):
+        if end_date is not None:
+            self._end_date = datetime.strptime(end_date, date_fmt_in)
+            self.nml['INPUT_ORG']['runctl']['ydate_end'] = self._end_date.strftime(date_fmt_cosmo)
+        elif 'ydate_end' in self.nml['INPUT_ORG']['runctl'].keys():
+            self._end_date = datetime.strptime(self.nml['INPUT_ORG']['runctl']['ydate_end'], date_fmt_cosmo)
+        else:
+            self._end_date = None
+
+    @property
+    def account(self):
+        return self._account
+    @account.setter
+    def account(self, acc):
+        if acc is None:
+            # Guess from ${PROJECT} environment variable
+            self._account = os.path.normpath(os.environ['PROJECT']).split(os.path.sep)[-2]
+        else:
+            self._account = acc
+
+
+    # =======
+    # Methods
+    # =======
+    def _organize_tasks(self, ncosx, ncosy, ncosio, ncesm):
+        # COSMO tasks
+        # -----------
+        if ncosx is None:
+            self._ncosx = self.nml['INPUT_ORG']['runctl']['nprocx']
+        else:
+            self._ncosx = ncosx
+            self.nml['INPUT_ORG']['runctl']['nprocx'] = ncosx
+        if ncosy is None:
+            self._ncosy = self.nml['INPUT_ORG']['runctl']['nprocy']
+        else:
+            self._ncosy = ncosy
+            self.nml['INPUT_ORG']['runctl']['nprocy'] = ncosy
+        if ncosio is None:
+            self._ncosio = self.nml['INPUT_ORG']['runctl']['nprocio']
+        else:
+            self._ncosio = ncosio
+            self.nml['INPUT_ORG']['runctl']['nprocio'] = ncosio
+        self._ncos = self._ncosx * self._ncosy + self._ncosio
+        # CESM tasks and number of nodes
+        # ------------------------------
+        if self.cosmo_only:
+            self._ncesm = 0
+            if self.gpu_mode:
+                self._n_nodes = self._ncos
+            else:
+                self._n_nodes = self._ncos // self._n_tasks_per_node
+        else:
+            if self.gpu_mode:   # Populate nodes with CESM tasks except one
+                self._n_nodes = self._ncos
+                self._ncesm = self._n_nodes * (self._n_tasks_per_node - 1)
+            else:   # Determine number of CESM tasks and deduce number of nodes
+                if ncesm is None:
+                    self._ncesm = self.nml['drv_in']['ccsm_pes']['lnd_ntasks']
+                else:
+                    self._ncesm = ncesm
+                ntot = self._ncos + self._ncesm
+                if ntot % self._n_tasks_per_node != 0:
+                    msg = "total number of tasks (ncosx x ncosy + ncosio + ncesm = {:d}) has to be divisible by {:d}"
+                    raise ValueError(msg.format(ntot, self._n_tasks_per_node))
+                self._n_nodes = ntot // self._n_tasks_per_node
+            # Apply number of CESM tasks to all relevant namelist parameters
+            for comp in ['atm', 'cpl', 'glc', 'ice', 'lnd', 'ocn', 'rof', 'wav']:
+                self.nml['drv_in']['ccsm_pes']['{:s}_ntasks'.format(comp)] = self._ncesm
+
+
+    def _compute_run_dates(self):
+        # Access to namelists
+        # -------------------
+        INPUT_ORG = self.nml['INPUT_ORG']
+        if not self.cosmo_only:
+            drv_in = self.nml['drv_in']
+        # Read in _run_start_date
+        # -----------------------
+        date_cosmo = datetime.strptime(INPUT_ORG['runctl']['ydate_ini'], date_fmt_cosmo) \
+                     + timedelta(hours=INPUT_ORG['runctl']['hstart'])
+        if not self.cosmo_only:
+            date_cesm = datetime.strptime(str(drv_in['seq_timemgr_inparm']['start_ymd']), date_fmt_cesm)
+            if date_cosmo != date_cesm:
+                raise ValueError("start dates are not identical in COSMO and CESM namelists")
+        self._run_start_date = date_cosmo
+        # Compute _runtime and _run_end_date (possibly _end_date)
+        # -------------------------------------------------------
+        if self._end_date is not None:
+            if self._run_start_date > self._end_date:
+                raise ValueError("run sart date is larger than case end date")
+            elif self._run_start_date == self._end_date:
+                self._runtime = timedelta(days=1)
+                self._run_end_date = self._end_date + self._runtime
+            else:
+                if self.run_length is None:
+                    self._run_end_date = self._end_date
+                else:
+                    self._run_end_date = min(add_time_from_str(self._run_start_date, self.run_length),
+                                             self._end_date)
+                self._runtime = self._run_end_date - self._run_start_date
+        else:
+            if self.run_length is None:
+                runtime_cosmo = (INPUT_ORG['runctl']['nstop'] + 1) * INPUT_ORG['runctl']['dt'] \
+                                - INPUT_ORG['runctl']['hstart'] * 3600.0
+                if not self.cosmo_only:
+                    runtime_cesm = drv_in['seq_timemgr_inparm']['stop_n']
+                    if runtime_cosmo != runtime_cesm:
+                        raise ValueError("run lengths are not identical in COSMO and CESM namelists")
+                self._runtime = timedelta(seconds=runtime_cosmo)
+                self._run_end_date = self._run_start_date + self._runtime
+            else:
+                self._run_end_date = add_time_from_str(self._run_start_date, self.run_length)
+                self._runtime = self._run_end_date - self._run_start_date
+            self._end_date = self._run_end_date
+
+
+    def _apply_run_dates(self):
+        # Compute times
+        hstart = (self._run_start_date - self.start_date).total_seconds() // 3600.0
+        runtime_seconds = self._runtime.total_seconds()
+        runtime_hours = runtime_seconds // 3600.0
+        hstop = hstart + runtime_hours
+        # Access to namelists
+        INPUT_ORG = self.nml['INPUT_ORG']
+        INPUT_IO = self.nml['INPUT_IO']
+        if not self.cosmo_only:
+            drv_in = self.nml['drv_in']
+        # adapt INPUT_ORG
+        INPUT_ORG['runctl']['nstop'] = int(hstop * 3600.0 // INPUT_ORG['runctl']['dt']) - 1
+        # adapt INPUT_IO
+        for gribout in self._get_gribouts():
+            gribout['hcomb'][0:2] = hstart, hstop
+        INPUT_IO['ioctl']['nhour_restart'] = [int(hstop), int(hstop), 24]
+        if not self.cosmo_only:
+            # adapt drv_in
+            drv_in['seq_timemgr_inparm']['stop_n'] = int(runtime_seconds)
+            drv_in['seq_timemgr_inparm']['restart_n'] = int(runtime_seconds)
+            # adapt namcouple
+            with open(os.path.join(self.path, 'namcouple_tmpl'), mode='r') as f:
+                content = f.read()
+            content = re.sub('_runtime_', str(int(self._runtime.total_seconds())), content)
+            with open(os.path.join(self.path, 'namcouple'), mode='w') as f:
+                f.write(content)
+
+
+    def _check_gribout(self):
+        # Only keep gribout blocks that fit within runtime
+        # (essentially to avoid crash for short tests)
+        runtime_hours = self._runtime.total_seconds() // 3600.0
+        gribouts_out = []
+        gribouts_in = self._get_gribouts()
+        for gribout in gribouts_in:
+            if runtime_hours >= gribout['hcomb'][2]:
+                gribouts_out.append(gribout)
+        if gribouts_out:
+            self.nml['INPUT_IO']['gribout'] = gribouts_out
+            self.nml['INPUT_IO']['ioctl']['ngribout'] = len(gribouts_out)
+        else:
+            if gribouts_in:
+                del self.nml['INPUT_IO']['gribout']
+
+
+    def _get_gribouts(self):
+        if 'gribout' not in self.nml['INPUT_IO'].keys():
+            return []
+        else:
+            gribouts = self.nml['INPUT_IO']['gribout']
+            if not isinstance(gribouts, list):
+                gribouts = [gribouts]
+            return gribouts
+
+
+    def write_open_nml(self):
+        self.nml.write_all()
+
+
+    def _create_missing_dirs(self):
+        # COSMO
+        # -----
+        # input
+        self._mk_miss_path(self.nml['INPUT_IO']['gribin']['ydirini'])
+        self._mk_miss_path(self.nml['INPUT_IO']['gribin']['ydirbd'])
+        # output
+        for gribout in self._get_gribouts():
+            self._mk_miss_path(gribout['ydir'])
+        self._mk_miss_path(self.nml['INPUT_IO']['ioctl']['ydir_restart_in'])
+        self._mk_miss_path(self.nml['INPUT_IO']['ioctl']['ydir_restart_out'])
+        # CESM
+        # ----
+        if not self.cosmo_only:
+            # timing
+            # - ML - remove if exists before creating
+            shutil.rmtree(os.path.join(self.path, self.nml['drv_in']['seq_infodata_inparm']['timing_dir']),
+                          ignore_errors=True)
+            shutil.rmtree(os.path.join(self.path, self.nml['drv_in']['seq_infodata_inparm']['tchkpt_dir']),
+                          ignore_errors=True)
+            self._mk_miss_path(self.nml['drv_in']['seq_infodata_inparm']['timing_dir'])
+            self._mk_miss_path(self.nml['drv_in']['seq_infodata_inparm']['tchkpt_dir'])
+            # input / output
+            for comp in ['atm', 'cpl', 'glc', 'ice', 'lnd', 'ocn', 'rof', 'wav']:
+                self._mk_miss_path(self.nml['{:s}_modelio.nml'.format(comp)]['modelio']['diri'])
+                self._mk_miss_path(self.nml['{:s}_modelio.nml'.format(comp)]['modelio']['diro'])
+
+
+    def _mk_miss_path(self, rel_path):
+        path = os.path.join(self.path, rel_path)
+        if not os.path.exists(path):
+            print('Creating path ' + path)
+            os.makedirs(path)
+
+
+    def _build_proc_config(self):
+        """Place holder for method implemented by machine specific classes"""
+        raise ValueError("Method not implemented")
+
+
+    def _build_controller(self):
+        """Place holder for method implemented by machine specific classes"""
+        raise ValueError("Method not implemented")
+
+
+    def to_xml(self, file_name):
+
+        def indent(elem, level=0):
+            i = "\n" + level*"  "
+            if len(elem):
+                if not elem.text or not elem.text.strip():
+                    elem.text = i + "  "
+                if not elem.tail or not elem.tail.strip():
+                    elem.tail = i
+                for elem in elem:
+                    indent(elem, level+1)
+                if not elem.tail or not elem.tail.strip():
+                    elem.tail = i
+            else:
+                if level and (not elem.tail or not elem.tail.strip()):
+                    elem.tail = i
+
+        config = ET.Element('config')
+        tree = ET.ElementTree(config)
+        ET.SubElement(config, 'name').text = self.name
+        ET.SubElement(config, 'path').text = self.path
+        ET.SubElement(config, 'cosmo_only', attrib={'type': 'bool'}).text = '1' if self.cosmo_only else ''
+        ET.SubElement(config, 'start_date').text = self.start_date.strftime(date_fmt_in)
+        ET.SubElement(config, 'end_date').text = self.end_date.strftime(date_fmt_in)
+        ET.SubElement(config, 'run_length').text = self.run_length
+        ET.SubElement(config, 'COSMO_exe').text = self.COSMO_exe
+        if not self.cosmo_only:
+            ET.SubElement(config, 'CESM_exe').text = self.CESM_exe
+        ET.SubElement(config, 'wall_time').text = self.wall_time
+        ET.SubElement(config, 'account').text = self.account
+        if self.partition is not None:
+            ET.SubElement(config, 'partition').text = self.partition
+        ET.SubElement(config, 'gpu_mode', attrib={'type': 'bool'}).text = '1' if self.gpu_mode else ''
+        if self.modules_opt is not None:
+            ET.SubElement(config, 'modules_opt').text = self.modules_opt
+        if self.pgi_version is not None:
+            ET.SubElement(config, 'pgi_version').text = self.pgi_version
+        ET.SubElement(config, 'login_shell', attrib={'type': 'bool'}).text = '1' if self.login_shell else ''
+        ET.SubElement(config, 'dummy_day', attrib={'type': 'bool'}).text = '1' if self.dummy_day else ''
+        indent(config)
+        tree.write(os.path.join(self.path, file_name), xml_declaration=True)
+
+
+    def set_next_run(self):
+        if ((self._run_start_date >= self._end_date) or
+            (self._run_end_date == self._end_date and not self.dummy_day)):
+            return False
+        else:
+            hstart = (self._run_end_date - self._start_date).total_seconds() // 3600.0
+            self.nml['INPUT_ORG']['runctl']['hstart'] = hstart
+            if not self.cosmo_only:
+                self.nml['drv_in']['seq_timemgr_inparm']['start_ymd'] = int(self._run_end_date.strftime(date_fmt_cesm))
+            self._compute_run_dates()
+            # - ML - Setting ydirini might not be needed, try without at some point
+            self.nml['INPUT_IO']['gribin']['ydirini'] = self.nml['INPUT_IO']['ioctl']['ydir_restart_out']
+            for gribout in self._get_gribouts():
+                gribout['lwrite_const'] = False
+            if not self.cosmo_only:
+                self.nml['drv_in']['seq_infodata_inparm']['start_type'] = 'continue'
+            self.write_open_nml()
+            self._update_controller()
+            return True
+
+
+    def _update_controller(self):
+        """Place holder for method implemented by machine specific classes"""
+        raise ValueError("Method not implemented")
+
+
+    def submit(self):
+        """Place holder for method implemented by machine specific classes"""
+        raise ValueError("Method not implemented")
+
+
+    def run(self):
+        """Place holder for method implemented by machine specific classes"""
+        raise ValueError("Method not implemented")
+# base case class:1 ends here
+
+# [[file:~/Projects/COSMO_CLM2_tools/COSMO_CLM2_tools.org::*Daint%20case%20class][Daint case class:1]]
+class daint_case(base_case):
+    """Class defining a COSMO-CLM2 case on Piz Daint"""
 
     # Class wide variables
     # ====================
@@ -506,7 +899,7 @@ class case(object):
         elapsed = time.time() - start_time
         print("\nCase {name:s} ran in {elapsed:.2f}\n".format(name=self.name, elapsed=elapsed))
         os.chdir(cwd)
-# class case:1 ends here
+# Daint case class:1 ends here
 
 # [[file:~/Projects/COSMO_CLM2_tools/COSMO_CLM2_tools.org::*class%20nmldict][class nmldict:1]]
 class nmldict(dict):
