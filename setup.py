c8f1f424c7fbb5cbd4c44b4151e5cc3ebbc1d032
Matthieu Leclair <matthieu.leclair@env.ethz.ch>
2018-10-22 16:52:41 +0200

    First try to split the one-file source

--- setup.py
+++ setup.py
@@ -5,14 +5,22 @@ from setuptools import setup
 # except ImportError:
 #     from distutils.core import setup
 
+def get_version():
+    with open('COSMO_CLM2_tools/__init__.py') as f:
+        for line in f:
+            if line.startswith('__version__'):
+                _, _, version = line.replace("'", '').split()
+                break
+    return version
+
 setup(name='COSMO_CLM2_tools',
-      version='0.2',
+      version=get_version(),
       description="python based tools to set up a COSMO_CLM2 case",
       author="Matthieu Leclair",
       author_email="matthieu.leclair@env.ethz.ch",
       url="https://github.com/COSMO-RESM/COSMO-CLM2_tools",
       packages=['COSMO_CLM2_tools'],
-      entry_points={'console_scripts': ['cc2_create_case = COSMO_CLM2_tools.cosmo_clm2:create_new_case',
-                                        'cc2_control_case = COSMO_CLM2_tools.cosmo_clm2:control_case']},
+      entry_points={'console_scripts': ['cc2_create_case = COSMO_CLM2_tools.create_case:create_case',
+                                        'cc2_control_case = COSMO_CLM2_tools.control_case:control_case']},
       install_requires=['f90nml>=1.0.2']
 )
